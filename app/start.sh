1ac764c4f4e7e15d87774cff64bf77e690bc8401
Kamil Kaminski <kamilkaminski39@gmail.com>
2023-04-28 01:49:56 +0200

    Added migration command to start.sh

--- app/start.sh
+++ app/start.sh
@@ -3,6 +3,9 @@
 echo "Collecting static files"
 python manage.py collectstatic --no-input
 
+echo "Migrating"
+python manage.py migrate
+
 echo "Starting server"
 gunicorn -b 0.0.0.0 -p 8000 app.wsgi:application
 
