917f806b2993538ea53660e480a53a28d2c818ac
Kamil Kaminski <kamilkaminski39@gmail.com>
2023-04-28 01:05:51 +0200

    Switched ports back to 8000

--- app/start.sh
+++ app/start.sh
@@ -4,6 +4,6 @@ echo "Collecting static files"
 python manage.py collectstatic --no-input
 
 echo "Starting server"
-gunicorn -b 0.0.0.0 -p 80 app.wsgi:application
+gunicorn -b 0.0.0.0 -p 8000 app.wsgi:application
 
 exec "$@"
